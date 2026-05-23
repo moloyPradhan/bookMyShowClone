a07cebca9f9ceab6496dbf1d9a1a5fd53eeedf59
Moloy Pradhan <pradhanmoloy15@gmail.com>
2026-05-23 21:09:22 +0530

    smtp main setup done

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,90 +6,55 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
-    public string $recipients = '';
 
-    /**
-     * The "user agent"
-     */
+    public string $fromEmail;
+    public string $fromName;
+
+    public string $protocol;
+
+    public string $SMTPHost;
+    public string $SMTPUser;
+    public string $SMTPPass;
+    public int $SMTPPort;
+
+    public string $SMTPCrypto;
+
+    public string $mailType = 'html';
+    public string $charset = 'utf-8';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->fromEmail = env('email.fromEmail');
+        $this->fromName = env('email.fromName');
+
+        $this->protocol = env('email.protocol', 'smtp');
+
+        $this->SMTPHost = env('email.SMTPHost');
+        $this->SMTPUser = env('email.SMTPUser');
+        $this->SMTPPass = env('email.SMTPPass');
+
+        $this->SMTPPort = (int) env(
+            'email.SMTPPort',
+            587
+        );
+
+        $this->SMTPCrypto = env(
+            'email.SMTPCrypto',
+            'tls'
+        );
+    }
+
     public string $userAgent = 'CodeIgniter';
-
-    /**
-     * The mail sending protocol: mail, sendmail, smtp
-     */
-    public string $protocol = 'mail';
-
-    /**
-     * The server path to Sendmail.
-     */
     public string $mailPath = '/usr/sbin/sendmail';
-
-    /**
-     * SMTP Server Hostname
-     */
-    public string $SMTPHost = '';
-
-    /**
-     * SMTP Username
-     */
-    public string $SMTPUser = '';
-
-    /**
-     * SMTP Password
-     */
-    public string $SMTPPass = '';
-
-    /**
-     * SMTP Port
-     */
-    public int $SMTPPort = 25;
-
-    /**
-     * SMTP Timeout (in seconds)
-     */
     public int $SMTPTimeout = 5;
-
-    /**
-     * Enable persistent SMTP connections
-     */
     public bool $SMTPKeepAlive = false;
-
-    /**
-     * SMTP Encryption.
-     *
-     * @var string '', 'tls' or 'ssl'. 'tls' will issue a STARTTLS command
-     *             to the server. 'ssl' means implicit SSL. Connection on port
-     *             465 should set this to ''.
-     */
-    public string $SMTPCrypto = 'tls';
-
-    /**
-     * Enable word-wrap
-     */
     public bool $wordWrap = true;
-
-    /**
-     * Character count to wrap at
-     */
     public int $wrapChars = 76;
-
-    /**
-     * Type of mail, either 'text' or 'html'
-     */
-    public string $mailType = 'text';
-
-    /**
-     * Character set (utf-8, iso-8859-1, etc.)
-     */
-    public string $charset = 'UTF-8';
-
-    /**
-     * Whether to validate the email address
-     */
     public bool $validate = false;
 
-    /**
+    /** 
      * Email Priority. 1 = highest. 5 = lowest. 3 = normal
      */
     public int $priority = 3;
